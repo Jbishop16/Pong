93f121bb79cf15282b47def8246252df866e3576
Jbishop16 <Jrbishop16@yahoo.com>
2016-03-13 19:30:22 -0400

    Added files via upload

---
+++ Pong/Assets/GameSetup.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var mainCam : Camera;
+
+var topWall : BoxCollider2D;
+var bottomWall : BoxCollider2D;
+var leftWall : BoxCollider2D;
+var rightWall : BoxCollider2D;
+
+var Player01 : Transform;
+var Player02 : Transform;
+
+function Update () {
+
+	//Move each wall to its edge location
+	topWall.size = new Vector2 (mainCam.ScreenToWorldPoint, 1f);
+
+}
